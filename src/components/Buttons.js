8042bb556dac7118c81b3b5f1f6d5b82512af207
JadenFicklin <jadendficklin@gmail.com>
2022-04-15 22:41:44 -0600

    progress bar

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import "../styles/Buttons.css";
 
 function Buttons() {
-  return <div>Buttons</div>;
+  const [number, setNumber] = useState(100);
+  if (number > 100) {
+    setNumber(100);
+  }
+  if (number < 0) {
+    setNumber(0);
+  }
+
+  return (
+    <>
+      <div className="value-box">
+        <div className="value" style={{ width: `${number}%` }}></div>
+      </div>
+      <h4>{number}</h4>
+      <button onClick={() => setNumber(number - 10)}>-1</button>
+      <button onClick={() => setNumber(number + 10)}>+1</button>
+    </>
+  );
 }
 
 export default Buttons;
