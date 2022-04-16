643ea21882088bcde1f781e5c1a6213bb52a4773
JadenFicklin <jadendficklin@gmail.com>
2022-04-15 23:52:34 -0600

    game over and play again option

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -24,7 +24,7 @@ function Buttons() {
   const handleWork = () => {
     setMoney(money + 20);
     setDay(day + 1);
-    setFood(food - 20);
+    setFood(food - 30);
     setWarmth(warmth - levelOfWarmth);
   };
   const handleFood = () => {
@@ -41,34 +41,51 @@ function Buttons() {
     setLevelOfWarmth(10);
   };
 
-  return (
-    <>
-      Food
-      <div className="value-box">
-        <div className="value" style={{ width: `${food}%` }}></div>
-      </div>
-      Warmth
-      <div className="value-box">
-        <div className="value" style={{ width: `${warmth}%` }}></div>
-      </div>
-      <div className="money">day {day}</div>
-      <div className="money">savings: ${money}</div>
-      <div className="shop-box">
-        <p>
-          + $20 <button onClick={() => handleWork()}>work for a day</button>
-          <br></br>
-          <br></br>- $5
-          <button onClick={() => handleFood()}>buy and eat Food</button>
-          <br></br>
-          <br></br> <span>Free</span>
-          <button onClick={() => handleFire()}>sit by fire</button>
-          <br></br>
-          <br></br> <span>- $100</span>
-          <button onClick={() => handleJacket()}>buy jacket</button>
-        </p>
-      </div>
-    </>
-  );
+  if (food === 0 || warmth === 0) {
+    return (
+      <>
+        <div>you died</div>
+        <br></br>
+        <button
+          onClick={() => {
+            setFood(100);
+            setWarmth(100);
+          }}
+        >
+          Play again
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        Food
+        <div className="value-box">
+          <div className="value" style={{ width: `${food}%` }}></div>
+        </div>
+        Warmth
+        <div className="value-box">
+          <div className="value" style={{ width: `${warmth}%` }}></div>
+        </div>
+        <div className="money">day {day}</div>
+        <div className="money">savings: ${money}</div>
+        <div className="shop-box">
+          <p>
+            + $20 <button onClick={() => handleWork()}>work for a day</button>
+            <br></br>
+            <br></br>- $5
+            <button onClick={() => handleFood()}>buy and eat Food</button>
+            <br></br>
+            <br></br> <span>Free</span>
+            <button onClick={() => handleFire()}>sit by fire</button>
+            <br></br>
+            <br></br> <span>- $100</span>
+            <button onClick={() => handleJacket()}>buy jacket</button>
+          </p>
+        </div>
+      </>
+    );
+  }
 }
 
 export default Buttons;
